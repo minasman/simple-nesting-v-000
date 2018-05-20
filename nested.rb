946d20d436d9640e2c0aabbeee31f04b59435326
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-20 20:03:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -115,8 +114,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  binding.pry 
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  binding.pry 
 end
-adding_to_dennis
