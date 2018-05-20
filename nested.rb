8b37f00fa8fa3b6d8d84bf8a95b6a539caef5c66
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-20 20:05:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -75,6 +75,7 @@ def adding_matz
         }
      }
   programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
+  programmer_hash
 end
 
 def changing_alan
@@ -96,6 +97,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -115,4 +117,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
